ca5de4e74117faca57643bb030090e70b3d73099
Matic <matic18@protonmail.com>
2025-04-15 13:24:36 +0200

    feat(ios): deinit only with remove observer

--- ios/Classes/PDFCoreView.swift
+++ ios/Classes/PDFCoreView.swift
@@ -108,6 +108,12 @@ class PDFCoreViewController: UIViewController, UIGestureRecognizerDelegate {
             }
         }
     }
+
+    deinit {
+        print("deinit called - removing observer")
+        // remove strong reference to pdfView
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.PDFViewPageChanged, object: self.pdfView)
+    }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
